0e98ad6cb1a9caedfdd82c78696c1b135edb68bd
Samer <samer_control@yahoo.com>
2017-07-09 21:15:18 -0400

    routing init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
+// Angular modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+// App
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+
 import { HeaderComponentComponent } from './header-component/header-component.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: 'users', component: UsersComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponentComponent
+    HeaderComponentComponent,
+    UsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+    // other imports here
   ],
   providers: [],
   bootstrap: [AppComponent]
